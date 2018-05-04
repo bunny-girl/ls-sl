7e3be432d610ea8dc3574741c5dfd250653c3f78
Edel Ma <qun_ma@apple.com>
2018-05-04 10:54:12 +0800

    Update base64 things

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-export default {
+const Base64 = require('js-base64');
+
+module.exports = {
+    base64 : false,
     save(key, value) {
         if(this.validate(key)){
             try{
+                let tempVal = value;
                 switch (typeof value){
                     case 'number' :
                     case 'undefined' :
                     case 'string' :
-                        localStorage.setItem(key, value);
                         break;
                     default:
-                        localStorage.setItem(key, JSON.parse(value))
+                        tempVal = JSON.parse(value);
                 }
+                if(this.base64){
+                    tempVal = Base64.encode(tempVal);
+                }
+                localStorage.setItem(key, tempVal);
                 return true;
             }
             catch(e){
@@ -21,9 +28,14 @@ export default {
         }
     },
     load(key) {
-        if(this.validate(key)){
-            return localStorage.getItem(key);
-        }else{
+        try{
+            if(this.validate(key)){
+                return Base64.decode(localStorage.getItem(key));
+            }else{
+                return null;
+            }
+        }
+        catch(e){
             return null;
         }
     },
